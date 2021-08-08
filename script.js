bbeaa444456f31bef9a7cb20244faec0ac2497dc
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2021-08-08 22:52:10 +0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// selecting elements
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const currentScore0El = document.getElementById("current--0");
+const currentScore1El = document.getElementById("current--1");
