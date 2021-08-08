b39b4c8b66fe87ce577ac8c7d97fc5a71973aa58
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2021-08-08 22:52:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 const currentScore0El = document.getElementById("current--0");
 const currentScore1El = document.getElementById("current--1");
+
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const diceEl = document.querySelector(".dice");
+const msg0El = document.querySelector(".wining-msg--0");
+const msg1El = document.querySelector(".wining-msg--1");
